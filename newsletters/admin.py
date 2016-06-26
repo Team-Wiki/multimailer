cb9bd4a7afb6c9caed70e9d7a9d1b0427721ff4b
Max Weller <git@max-weller.de>
2016-06-27 00:01:54 +0200

    add admin

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from newsletters.models import PlaintextDraft, Shortlink, Newsletter, Edition, Message
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PlaintextDraft)
+class PlaintextDraftAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Edition)
+class EditionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Shortlink)
+class ShortlinkAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
